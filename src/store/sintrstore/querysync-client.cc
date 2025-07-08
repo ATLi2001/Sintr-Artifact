89f9b2377cfaa14f12c4931d6e7fd2d0f62c834b
Daniel Lee <danielhlee02@gmail.com>
2025-07-08 19:36:55 -0400

    Fixed req id bug with processread

--- src/store/sintrstore/querysync-client.cc
+++ src/store/sintrstore/querysync-client.cc
@@ -1186,7 +1186,7 @@ void ShardClient::HandlePointQueryResult(proto::PointQueryResultReply &queryResu
     PendingQuorumGet *req = &pendingQuery->pendingPointQuery;
 
     const proto::CommittedProof *proof = queryResult.has_proof() ? &queryResult.proof() : nullptr;
-    bool finished = ProcessRead(queryReq.req_id(), req, read_t::POINT, write, queryResult.has_proof(), proof, queryResult);
+    bool finished = ProcessRead(queryResult.req_id(), req, read_t::POINT, write, queryResult.has_proof(), proof, queryResult);
 
     if(finished){
         query_seq_num_mapping.erase(pendingQuery->query_seq_num);
