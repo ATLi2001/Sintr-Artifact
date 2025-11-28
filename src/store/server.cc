7da32a56a94da375f126edde6447d1215607d205
Daniel Lee <danielhlee02@gmail.com>
2025-11-28 14:14:38 -0500

    added fixes for hotstuff and bftsmart in Pesto repo

--- src/store/server.cc
+++ src/store/server.cc
@@ -593,7 +593,7 @@ int main(int argc, char **argv) {
 
   int threadpool_mode = 0; //default for Basil.   //|| proto == PROTO_PELOTON_SMR
   if(proto == PROTO_HOTSTUFF || proto == PROTO_AUGUSTUS) threadpool_mode = 1;
-  if(proto == PROTO_BFTSMART || proto == PROTO_AUGUSTUS_SMART) threadpool_mode = 0;
+  if(proto == PROTO_BFTSMART || proto == PROTO_AUGUSTUS_SMART) threadpool_mode = 2;
   if((proto == PROTO_PEQUIN || proto == PROTO_SINTR) && FLAGS_sql_bench) threadpool_mode = 0;
   // if(proto == PROTO_PG_SMR) threadpool_mode = 3;
 
@@ -1056,7 +1056,7 @@ int main(int argc, char **argv) {
 
       // HotStuff
   case PROTO_HOTSTUFF: {
-      
+      Notice("Using [%s] server config", FLAGS_local_config ? "LOCAL" : "REMOTE");
       server = new hotstuffstore::Server(config, &keyManager,
                                      FLAGS_group_idx, FLAGS_replica_idx, FLAGS_num_shards, FLAGS_num_groups,
                                      FLAGS_indicus_sign_messages, FLAGS_indicus_validate_proofs, sintr_params,
@@ -1064,7 +1064,7 @@ int main(int argc, char **argv) {
 																	   FLAGS_pbft_order_commit, FLAGS_pbft_validate_abort);
 
       replica = new hotstuffstore::Replica(config, &keyManager,
-                                       dynamic_cast<hotstuffstore::App *>(server),
+                                       dynamic_cast<hotstuffstore::App *>(server), FLAGS_local_config,
                                        FLAGS_group_idx, FLAGS_replica_idx, FLAGS_indicus_sign_messages,
                                        FLAGS_indicus_sign_client_proposals,
                                        FLAGS_indicus_sig_batch, FLAGS_indicus_sig_batch_timeout,
