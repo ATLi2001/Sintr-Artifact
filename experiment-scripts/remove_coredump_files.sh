2faa70b6abfa86f8314fe19a4ddcbdc6bf1605b1
Austin Li <austintianli@gmail.com>
2026-03-17 12:00:23 -0400

    coredump script setup for clients

--- experiment-scripts/remove_coredump_files.sh
+++ experiment-scripts/remove_coredump_files.sh
@@ -2,8 +2,9 @@
 
 # === Server list and related config (simplified from your larger script) ===
 declare -a arr_servers=("us-east-1-0" "us-east-1-1" "us-east-1-2" "eu-west-1-0" "eu-west-1-1" "eu-west-1-2")
-USER="dhl001"
-EXP_NAME="sintr-rw"
+declare -a arr_clients=("client-0-0" "client-1-0" "client-2-0" "client-3-0" "client-4-0" "client-5-0" "client-0-1" "client-1-1" "client-2-1" "client-3-1" "client-4-1" "client-5-1")
+USER="atli"
+EXP_NAME="sintr"
 PROJECT_NAME="pequin"
 CLUSTER_NAME="utah"
 
@@ -12,5 +13,13 @@ REMOTE_DIR="/mnt/extra/coredump"
 for host in "${arr_servers[@]}"; do
     full_host="${USER}@${host}.${EXP_NAME}.${PROJECT_NAME}-pg0.${CLUSTER_NAME}.cloudlab.us"
     echo "Deleting on $full_host"
-    ssh "$full_host" "rm -rf $REMOTE_DIR"
+    # ssh "$full_host" "sudo sysctl -w kernel.core_pattern=$REMOTE_DIR/core.%p.%t"
+    ssh "$full_host" "rm -rf $REMOTE_DIR; mkdir -p $REMOTE_DIR"
 done
+
+# for host in "${arr_clients[@]}"; do
+#     full_host="${USER}@${host}.${EXP_NAME}.${PROJECT_NAME}-pg0.${CLUSTER_NAME}.cloudlab.us"
+#     echo "Deleting on $full_host"
+#     ssh "$full_host" "sudo sysctl -w kernel.core_pattern=$REMOTE_DIR/core.%p.%t"
+#     ssh "$full_host" "rm -rf $REMOTE_DIR; mkdir -p $REMOTE_DIR"
+# done
